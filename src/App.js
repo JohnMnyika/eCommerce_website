deb1bc95acbefc05562eb80fbc72e7282100fe58
Johnmnyika <jmnyika514@gmail.com>
2021-03-28 21:50:31 +0300

    Mock Products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Products from './components/Products/Products';
+
 
 const App = () => {
     return ( <
         div >
-        E - commerce website <
+        <
+        Products / >
+        <
         /div>
     )
 }
